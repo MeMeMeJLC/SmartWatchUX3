d84d778f7565774d8f1ae55d38f114f4475af318
Jeremy Cook <jeremy.cook.llb@live.com>
2016-11-01 15:50:08 +1300

    intial commit
    post goal working

---
+++ RefWatchUX3/MainActivity.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Wearable.Views;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+using Java.Interop;
+using Android.Views.Animations;
+using System.Net;
+using System.IO;
+
+namespace RefWatchUX3
+{
+    [Activity(Label = "RefWatchUX3", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var v = FindViewById<WatchViewStub>(Resource.Id.watch_view_stub);
+            v.LayoutInflated += delegate
+            {
+
+                // Get our button from the layout resource,
+                // and attach an event to it
+                Button button = FindViewById<Button>(Resource.Id.myButton);
+
+                button.Click += delegate
+                {
+                    var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://refwebapiodata20161016090651.azurewebsites.net/odata/Goal/");
+                    httpWebRequest.ContentType = "application/json";
+                    httpWebRequest.Method = "POST";
+
+                    using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                    {
+                        string json = "{ \"GamePlayerId\":20, \"IsOwnGoal\":false, \"GoalTime\":\"PT12M34S\"}";
+
+                        Console.WriteLine(json);
+
+                        streamWriter.Write(json);
+                        streamWriter.Flush();
+                        streamWriter.Close();
+                    }
+
+                    var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+                    }
+                };
+            };
+        }
+    }
+}
+
+
